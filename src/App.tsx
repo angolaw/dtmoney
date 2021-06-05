08bd7375e16710e37c7c206a8b2a190f15c5c8f9
Willian S <wsapcf@gmail.com>
2021-06-05 13:30:03 -0300

    componentize modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
 import { GlobalStyle } from './styles/global';
 import Modal from 'react-modal';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 //fix warning
 Modal.setAppElement('#root')
@@ -19,14 +20,11 @@ function App() {
       <Header handleOpenNewTransactionModal={handleOpenNewTransactionModal}  />
       <Dashboard/>
       <GlobalStyle/>
-      <Modal  
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar Transação</h2>
-
-
-      </Modal>
+      />
+      
     </>
   );
 }
