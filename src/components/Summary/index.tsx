5a79c102dee0f6b5bebe2eb74c1eb0d61678a190
Willian S <wsapcf@gmail.com>
2021-06-04 21:26:56 -0300

    fix icon alignment

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Container } from './styles'
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import totalImg from '../../assets/total.svg'
+
+export function Summary(){
+  return (
+    <Container>
+        <div>
+          <header>
+            <p>Entradas</p>
+            <img src={incomeImg} alt="income" />
+          </header>
+          <strong>R$ 10000,00</strong>
+        </div>
+        <div>
+          <header>
+            <p>Saidas</p>
+            <img src={outcomeImg} alt="outcome" />
+          </header>
+          <strong> -R$ 2500,00</strong>
+        </div>
+        <div className="highlight-background">
+          <header>
+            <p>Resumo</p>
+            <img src={totalImg} alt="total" />
+          </header>
+          <strong>R$ 7500,00</strong>
+        </div>
+    </Container>
+  )
+}
